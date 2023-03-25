234eb13f8d44a8016e5848ff28c1e661f86ef634
Alex Druzina <52024657+saashd@users.noreply.github.com>
2023-03-25 13:50:11 +0300

    Update getLocalIP.ts
    change localhost to your IP address to test on physical device.

--- client/App/utils/getLocalIP.ts
+++ client/App/utils/getLocalIP.ts
@@ -3,8 +3,10 @@ import { Platform } from "react-native";
 import { store } from "../store";
 import { startPollingData } from "../store/actions";
 
+
+// TODO: change localhost to your IP address to test on physical device.
 export const getIP = async () => {
-  const ip = Platform.OS === "android" ? "10.0.2.2" : "172.20.10.2";
+  const ip = Platform.OS === "android" ? "10.0.2.2" : "localhost";
   axios.defaults.baseURL = `http://${ip}:4000/api/`;
   store.dispatch(startPollingData());
 };
